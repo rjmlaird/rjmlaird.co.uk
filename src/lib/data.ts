5cb8e15e602ddc7916a7ebd653e4cf16dcc12d4d
rjmlaird <rjmlaird@gmail.com>
2026-07-10 17:46:25 +0100

    refresh

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,12 +1,10 @@
 import articles from '../data/articles.json';
-import publications from '../data/publications.json';
 import videos from '../data/videos.json';
 import nav from '../data/nav.json';
 import expertise from '../data/expertise.json';
 
 export {
   articles,
-  publications,
   videos,
   nav,
   expertise,
