f57db398fc9066d6be27f489096357f71d4b81ef
Ryan Laird <rjmlaird@users.noreply.github.com>
2026-07-20 21:30:16 +0100

    Simplify astro.config.mjs by removing unused code
    Removed unused imports and cloudflare adapter configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://rjmlaird.co.uk",
-  output: "server",
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
   scopedStyleStrategy: "class",
-  integrations: [icon(), mdx(), sitemap()],
+  integrations: [icon(),sitemap()],
 });
