8ea63127eea4ba272f9a441815c3228363f62bf1
Ryan Laird <rjmlaird@users.noreply.github.com>
2026-07-20 20:55:30 +0100

    Change output to server and add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://rjmlaird.co.uk",
-  output: "static",
+  output: "server",
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   scopedStyleStrategy: "class",
   integrations: [icon(), mdx(), sitemap()],
 });
